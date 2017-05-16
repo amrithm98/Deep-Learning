eeb4d323bf237e05f2ed86bd433115ab2cebdfe8
amrithm98 <amrithchallenger@gmail.com>
2017-05-16 17:09:00 +0530

    Pooling and Conv layer gen fns added

--- Convolution_Learning/convNN.py
+++ Convolution_Learning/convNN.py
@@ -14,5 +14,22 @@ display_step=50
 #network Parameters
 n_input=784 #28X28
 n_classes=10 #10 Digits
-dropout=0.75
+dropout=0.75	#deactivating neurons randomly -> prevents overfitting => Enabling it to generalize
+
+#Placeholders
+x=tf.placeholder(tf.float32,[None,n_input])
+y=tf.placeholder(tf.float32,[None,n_classes])
+keep_prob=tf.placeholder(tf.float32)	#for dropout
+
+#convolution Layer==>Increasing Abstraction
+def conv2d(x,W,b,strides=1):
+	x=tf.nn.conv2d(x,W,strides=[1,strides,strides,1],padding='SAME')
+	x=tf.nn.bias_add(x,b)
+	return tf.nn.relu(x)
+
+#Max Pooling Layer ==>Single Output from a pool taking maximum of the neurons
+def maxPool2d(x,k=2):
+	return tf.nn.max_pool(x,ksize=[1,k,k,1],strides=[1,k,k,1])
+
+
 
